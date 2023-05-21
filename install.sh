d206171d01d7c052fe3ee4915d2fbee56ec6a85e
Oscar <1570691+oscr@users.noreply.github.com>
2023-05-21 18:01:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,1 @@
-apt-get update && apt-get install -y lsb-release wget
-
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-apt update && apt install -y terraform
+echo "Hello World" >> tfplan
